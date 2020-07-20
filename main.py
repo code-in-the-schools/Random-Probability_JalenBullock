faa6872f47c1ccdf5f311ffca859ee83d98f87e7
JalenBullock <JalenB846@gmail.com>
2020-07-20 17:17:26 +0000

    More codes

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ import random
 randrange[10]
 
 for i in randrange(100):
+  r = random.randint(1, 100)
+  range.append(r)
+
+
+  for i in (randrange(len(range))
+    print(range[I]):
+
+
+
+
+
+
+
+import random
+
+randrange[1000]
+
+for i in randrange(10000):
   r = random.randint(1, 6)
   range.append(r)
 
 
   for i in (randrange(len(range))
-    print(ramdrange[i]):
+    print(ramdrange[I]):
+
+
 
 
 
