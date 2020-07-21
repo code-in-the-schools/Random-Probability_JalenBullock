62f423cede3b1f8a16659a00118e0bba3a77eea6
JalenBullock <JalenB846@gmail.com>
2020-07-21 14:49:07 +0000

    Improve my code

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 
 #create list 10
-ten = [10]
+Ten = []
 #create list 100
+hundred = []
 #create lis 100000
-
+Thousand = []
 #For loop for list 10
   #add rand num
 for i in range(10):
-  ten.append(random.randint(1,6)
-  print(ten[I])
+  Ten.append(random.randint(1,6)
+print(ten[i])
+#For loop for list 100
+  #add rand num
+for i in range(100):
+  Ten.append(random.randint(1,6))
+print(hundred[i])
+
+#for loop for list 100000
+  #add rand num
+for i in range(100000):
+  Ten.append(random.randint(1,6))
+  print(thousand[i])
