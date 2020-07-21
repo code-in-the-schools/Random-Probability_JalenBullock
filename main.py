8874e72ecf470e1ddebddfdf64858b3eae741bb3
JalenBullock <JalenB846@gmail.com>
2020-07-21 15:40:54 +0000

    More codes

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ Ten = []
 hundred = []
 #create lis 100000
 Thousand = []
+
 #For loop for list 10
   #add rand num
 for i in range(10):
   Ten.append(random.randint(1,6)
 print(ten[i])
+
 #For loop for list 100
   #add rand num
 for i in range(100):
@@ -21,3 +23,15 @@ print(hundred[i])
 for i in range(100000):
   Ten.append(random.randint(1,6))
   print(thousand[i])
+
+
+f = [1,2,3,4,5,6,7,8,]
+f_stats=[0,0,0,0,0,0]
+for i in range( len( f ) ):
+if f[i] ==1:
+  f_stats[0] +=1
+if f[i] ==2:
+  f_stats[1] +=1
+if f[i] ==1:
+  f_stats[0] +=1
+
